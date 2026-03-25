736f7776fdcab8fc91e225cee7c8d5566d8836e7
Joel Eulogio <joel.eulogio@utec.edu.pe>
2026-03-25 10:43:19 -0500

    verddera solucion

--- 1/primos.py
+++ 1/primos.py
@@ -2,14 +2,16 @@ def esPrimo(a):
     if a <= 1:
         return False
     
-    for i in range(2,a):
-        if a & i == 0:
+    for i in range(2, a):
+        if a % i == 0:
             return False
     return True
-def primos(a,b):
+
+def primos(a, b):
     listaDePrimos = []
-    for i in range(a,b+1):
-        if (esPrimo(i)):
+    for i in range(a, b+1):
+        if esPrimo(i):
             listaDePrimos.append(i)
     return listaDePrimos
-print(primos(10,20))
+
+print(primos(10, 20))
